a463b9cb6f03f33a52ab2127aba1c6cf4c395e2b
Ramzan9000 <ramzankhan7871@gmail.com>
2026-09-18 00:29:32 +0100

    Add CI runner infrastructure module

--- terraform/envs/ci-runner/main.tf
+++ terraform/envs/ci-runner/main.tf
@@ -1,9 +1,19 @@
 module "ci_vpc" {
   source = "../../modules/ci_vpc"
 
-  name               = var.name
-  cidr_block         = var.cidr_block
-  public_subnet_cidr = var.public_subnet_cidr
+  name                = var.name
+  cidr_block          = var.cidr_block
+  public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
-  availability_zone  = var.availability_zone
+  availability_zone   = var.availability_zone
+}
+
+module "ci_runner" {
+  source = "../../modules/ci_runner"
+
+  name      = "${var.name}-runner"
+  vpc_id    = module.ci_vpc.vpc_id
+  subnet_id = module.ci_vpc.private_subnet_id
+
+  depends_on = [module.ci_vpc]
 }
