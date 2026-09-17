3a1f8d5d1c4dbc273204b4f11c015ee7d59a8db3
Ramzan9000 <ramzankhan7871@gmail.com>
2026-09-18 00:32:58 +0100

    Add reusable VPC peering module

---
+++ terraform/modules/vpc_peering/main.tf
@@ -0,0 +1,34 @@
+resource "aws_vpc_peering_connection" "this" {
+  vpc_id      = var.requester_vpc_id
+  peer_vpc_id = var.accepter_vpc_id
+  auto_accept = true
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_vpc_peering_connection_options" "this" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.this.id
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
+
+resource "aws_route" "requester_to_accepter" {
+  route_table_id            = var.requester_route_table_id
+  destination_cidr_block    = var.accepter_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.this.id
+}
+
+resource "aws_route" "accepter_to_requester" {
+  for_each                  = toset(var.accepter_route_table_ids)
+  route_table_id            = each.value
+  destination_cidr_block    = var.requester_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.this.id
+}
