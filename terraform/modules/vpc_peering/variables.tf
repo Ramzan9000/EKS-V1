168d21b6fa569fa73ebb8cc8bbd3706a099cfb22
Ramzan9000 <ramzankhan7871@gmail.com>
2026-09-18 00:33:02 +0100

    Add reusable VPC peering module

---
+++ terraform/modules/vpc_peering/variables.tf
@@ -0,0 +1,34 @@
+variable "name" {
+  type        = string
+  description = "Name tag for the VPC peering connection."
+}
+
+variable "requester_vpc_id" {
+  type        = string
+  description = "Requester VPC ID."
+}
+
+variable "requester_vpc_cidr" {
+  type        = string
+  description = "CIDR block of the requester VPC."
+}
+
+variable "requester_route_table_id" {
+  type        = string
+  description = "Route table used by the requester subnet that needs peer access."
+}
+
+variable "accepter_vpc_id" {
+  type        = string
+  description = "Accepter VPC ID."
+}
+
+variable "accepter_vpc_cidr" {
+  type        = string
+  description = "CIDR block of the accepter VPC."
+}
+
+variable "accepter_route_table_ids" {
+  type        = set(string)
+  description = "Route tables in the accepter VPC that need a return route."
+}
