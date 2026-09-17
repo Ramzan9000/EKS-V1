9b1a4fd54b443bec00dbdc30890d54508978da1a
Ramzan9000 <ramzankhan7871@gmail.com>
2026-09-18 00:49:31 +0100

    Allow peering module to manage multiple route tables

--- terraform/modules/vpc_peering/main.tf
+++ terraform/modules/vpc_peering/main.tf
@@ -21,7 +21,8 @@ resource "aws_vpc_peering_connection_options" "this" {
 }
 
 resource "aws_route" "requester_to_accepter" {
-  route_table_id            = var.requester_route_table_id
+  for_each                  = toset(var.requester_route_table_ids)
+  route_table_id            = each.value
   destination_cidr_block    = var.accepter_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.this.id
 }
