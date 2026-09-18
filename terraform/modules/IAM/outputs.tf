0226465e7bb4dfd6738e1e2646ae5e70daf2fd29
Ramzan9000 <ramzankhan7871@gmail.com>
2026-09-18 06:07:47 +0100

    Expose EBS CSI IAM role ARN

--- terraform/modules/IAM/outputs.tf
+++ terraform/modules/IAM/outputs.tf
@@ -25,3 +25,7 @@ output "aws_load_balancer_controller_role_arn" {
 output "external_dns_role_arn" {
   value = aws_iam_role.external_dns.arn
 }
+
+output "ebs_csi_role_arn" {
+  value = aws_iam_role.ebs_csi.arn
+}
